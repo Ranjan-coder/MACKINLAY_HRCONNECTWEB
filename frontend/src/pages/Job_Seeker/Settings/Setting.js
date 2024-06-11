acc5310e1dbddb86353dc7864c77bc937f2a85d2
rishii64 <saptarsismmitra@gmail.com>
2024-06-11 11:52:33 +0530

    FE_Settings-Page Mods

--- frontend/src/pages/Job_Seeker/Settings/Setting.js
+++ frontend/src/pages/Job_Seeker/Settings/Setting.js
@@ -2,7 +2,8 @@
 import React, { useState } from 'react';
 import SettingStyle from '../Settings/Setting.module.css';
 import { useNavigate } from 'react-router-dom';
-import { FiUsers } from "react-icons/fi";
+// import { FiUsers } from "react-icons/fi";
+import { GoTrash } from "react-icons/go";
 import { FaArrowRightToBracket } from "react-icons/fa6";
 import { FaSync } from "react-icons/fa";
 import { RiUserSettingsFill } from "react-icons/ri";
@@ -17,13 +18,19 @@ function Setting() {
   const navi = useNavigate();
   const dispatch = useDispatch()
 
-  const handleLogoutClick = () => {
+  const handleLogOut = () => {
     dispatch(handleUserLogOut())
-    toast.success(`${name} logged out !!`)
+    toast.success(`${name} Logged out !!`)
     setTimeout(() => {
-      navi("/login")
+      navi('/dashboard')
     }, 1000);
   }
+
+  const [del, setDel] = useState(false)
+  const handleDeleteAccount = () => {
+    setDel(true)
+  }
+  const handlePopupClose = () => {setDel(false)}
 
   // Function to render content based on setting type
   const renderSettingContent = () => {
@@ -31,48 +38,41 @@ function Setting() {
       case "Setting/Profile":
         return (
           <div className={SettingStyle.profile_designs}>
-            {/* Content for Profile setting */}
-            <button className={SettingStyle.add_btn}>+ Add Profile</button>
-            <div className={SettingStyle.Profile_cont1}>
-              <div className={SettingStyle.me_profile_box}>
-                <img
-                  src={profileImage}
-                  alt="profile_img"
-                  onError={(e) => { e.target.src = `https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg`; e.onError = null; }}
-                />
-                <div className={SettingStyle.my_name_sec}>
-                  <h4>{name}</h4>
-                  <button onClick={() => { navi('/settings/editprofile') }}>Edit My Profile</button>
-                </div>
-                <div className={SettingStyle.logout_btn}>
-                  <p><FaArrowRightToBracket /></p>
-                  <span onClick={handleLogoutClick}>Log Out</span>
-                </div>
-              </div>
-              <div className={SettingStyle.manage_account}>
-                <hr></hr>
-                <p><span><FiUsers />
-                </span>Manage Account</p>
-              </div>
-
-            </div>
             <div className={SettingStyle.Profile_cont2}>
               <div className={SettingStyle.Profile_cont2_One}>
-                <p><FaSync /></p><span>Sync</span>
+                <p><FaSync /></p>
+                <span>Sync</span>
               </div>
               <div className={SettingStyle.Profile_cont2_One}>
-                <p><RiUserSettingsFill /></p><span>Profile Preference</span>
+                <p><RiUserSettingsFill /></p>
+                <span>Profile Preference</span>
               </div>
               <div className={SettingStyle.Profile_cont2_One}>
-                <p><TbUserExclamation /></p><span>Personal Info</span>
+                <p><TbUserExclamation /></p>
+                <span>Personal Info</span>
               </div>
             </div>
+
+            <div className={SettingStyle.Profile_cont1}>
+              <button className={SettingStyle.__pfEditBtn} onClick={() => { navi('/settings/editprofile') }}>Edit My Profile</button>
+              <button className={SettingStyle.__pfLogoutBtn} onClick={handleLogOut}> <FaArrowRightToBracket />Logout</button>
+              <button className={SettingStyle.__pfDeleteBtn} onClick={handleDeleteAccount}> <GoTrash /> Delete Account</button>
+            </div>
+            {
+              del && <div className={SettingStyle.__popupDelete}>
+                <p style={{fontWeight:"500", fontSize:"18px"}}>Are you sure you want to delete ??</p>
+                <p>
+                  <button className={SettingStyle.__btnAgree}>Agree</button>
+                  <button className={SettingStyle.__btnCancel} onClick={handlePopupClose}>Cancel</button>
+                </p>
+              </div>
+            }
+
           </div>
         );
       case "Setting/privacy":
         return (
           <div>
-            {/* Content for Privacy setting */}
             <h3>Privacy Settings</h3>
             <p>This is where you can manage your privacy preferences.</p>
           </div>
@@ -80,33 +80,24 @@ function Setting() {
       case "Setting/appearance":
         return (
           <div>
-            <button className={SettingStyle.add_btn}>+ Add Profile</button>
             <div className={`${SettingStyle.Appearance} ${SettingStyle.Profile_cont2}`}>
-
               <div className={SettingStyle.Profile_cont2_One}>
                 <span>Dark Mode</span>
               </div>
-
               <div className={SettingStyle.Profile_cont2_One}>
                 <span>Font Size</span>
               </div>
-
               <div className={SettingStyle.Profile_cont2_One}>
                 <span>Any other appearance button</span>
               </div>
-
-
             </div>
-
           </div>
         );
 
       case "Setting/notification":
         return (
           <div>
-            <button className={SettingStyle.add_btn}>+ Add Profile</button>
             <div className={`${SettingStyle.notification} ${SettingStyle.Profile_cont1}`}>
-
               <div className={SettingStyle.checkboxex}>
                 <label for='check'>gsbvcxbv</label>
                 <div className={SettingStyle.checkbox1}>
@@ -114,8 +105,6 @@ function Setting() {
                   <span>Allow Notification</span>
                 </div>
               </div>
-
-
               <div className={SettingStyle.checkboxex}>
                 <label for='check'>gsbvcxbv</label>
                 <div className={SettingStyle.checkbox1}>
@@ -123,8 +112,6 @@ function Setting() {
                   <span>Allow Notification</span>
                 </div>
               </div>
-
-
               <div className={SettingStyle.checkboxex}>
                 <label for='check'>gsbvcxbv</label>
                 <div className={SettingStyle.checkbox1}>
@@ -132,48 +119,36 @@ function Setting() {
                   <span>Allow Notification</span>
                 </div>
               </div>
-
             </div>
           </div>
-
-        );
-      case "Setting/reset":
-        return (
-          <div>
-
-          </div>
-
-        );
-      case "Setting/call":
-        return (
-          <div>
-
-          </div>
-
         );
 
-      case "Setting/help":
-        return (
-          <div>
+      // case "Setting/reset":
+      //   return (
+      //     <div>
+      //     </div>
+      //   );
+      // case "Setting/call":
+      //   return (
+      //     <div>
+      //     </div>
+      //   );
+      // case "Setting/help":
+      //   return (
+      //     <div>
+      //     </div>
+      //   );
 
-          </div>
-
-        );
       case "Setting/support":
         return (
           <div>
-
           </div>
-
         );
 
-
-
-      // Add cases for other setting types as needed
       default:
         return (
           <div>
-            <p>Select any option from the list</p>
+            <h5 className={SettingStyle.__defaultText}>Select any option from the list</h5>
           </div>
         );
     }
@@ -190,82 +165,37 @@ function Setting() {
         <h4 className={SettingStyle.__pfGreet}> {name}</h4>
       </div>
 
-      {/* <h2>Settings</h2> */}
       <div className={SettingStyle.Setting_opt_container}>
         <div className={SettingStyle.setting_opt_left}>
-          <div
-            className={settingtype === "Setting/Profile" ? `${SettingStyle.setting_opt_active} ` : `${SettingStyle.setting_opt}`}
-            onClick={() => setsettingtype("Setting/Profile")}
-          >
+          <div className={settingtype === "Setting/Profile" ? `${SettingStyle.setting_opt_active} ` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/Profile")}>
             <i className="fa-solid fa-user" /> Profile
           </div>
-          <div
-            className={settingtype === "Setting/privacy" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`}
-            onClick={() => setsettingtype("Setting/privacy")}
-          >
+          <div className={settingtype === "Setting/privacy" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/privacy")}>
             <i className="fa-solid fa-lock" /> Privacy & Service
           </div>
-          <div
-            className={settingtype === "Setting/appearance" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`}
-            onClick={() => setsettingtype("Setting/appearance")}
-          >
+          <div className={settingtype === "Setting/appearance" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/appearance")}>
             <i className="fa-solid fa-wand-magic-sparkles" /> Appearance
           </div>
-          <div
-            className={settingtype === "Setting/notification" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`}
-            onClick={() => setsettingtype("Setting/notification")}
-          >
+
+          {/* <div className={settingtype === "Setting/notification" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/notification")} >
             <i class="fa-solid fa-table-list" /> Notification
           </div>
-          <div
-            className={
-              settingtype === "Setting/reset"
-                ? `${SettingStyle.setting_opt_active}`
-                : `${SettingStyle.setting_opt}`
-            }
-            onClick={() => setsettingtype("Setting/reset")}
-          >
+          <div className={settingtype === "Setting/reset" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/reset")} >
             <i className="fa-solid fa-rotate-left" /> Switch to Candidate
           </div>
-          <div
-            className={
-              settingtype === "Setting/call"
-                ? `${SettingStyle.setting_opt_active}`
-                : `${SettingStyle.setting_opt}`
-            }
-            onClick={() => setsettingtype("Setting/call")}
-          >
+          <div className={settingtype === "Setting/call" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/call")} >
             <i className="fa-solid fa-phone" /> Call & message
           </div>
-          <div
-            className={
-              settingtype === "Setting/help"
-                ? `${SettingStyle.setting_opt_active}`
-                : `${SettingStyle.setting_opt}`
-            }
-            onClick={() => setsettingtype("Setting/help")}
-          >
+          <div className={settingtype === "Setting/help" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/help")}>
             <i className="fa-solid fa-info" /> Help
-          </div>
-          <div
-            className={
-              settingtype === "Setting/support"
-                ? `${SettingStyle.setting_opt_active}`
-                : `${SettingStyle.setting_opt}`
-            }
-            onClick={() => setsettingtype("Setting/support")}
-          >
+          </div> */}
+
+          <div className={settingtype === "Setting/support" ? `${SettingStyle.setting_opt_active}` : `${SettingStyle.setting_opt}`} onClick={() => setsettingtype("Setting/support")} >
             <i className="fa-solid fa-headset" /> Support
           </div>
-
-
-          {/* Add other setting options here */}
         </div>
         <div className={SettingStyle.setting_opt_right}>
-          <h2>{settingtype}</h2>
-          {/* Render the content based on setting type */}
           {renderSettingContent()}
-          {settingtype === "" && <p>Select any option from the list</p>}
         </div>
       </div>
     </div>
