39c91a66e5377b10c18c241e9e99a793cbf3c3c6
Kasthuri K <152369947+KasthuriK310@users.noreply.github.com>
2024-06-24 11:22:14 +0530

    new changes

--- frontend/src/pages/Job_Seeker/MyResume/MyResume.js
+++ frontend/src/pages/Job_Seeker/MyResume/MyResume.js
@@ -10,7 +10,8 @@ import Resume4 from "./canvaresume4.webp";
 import Resume5 from "./canvaresume5.webp";
 import Resume6 from "./canvaresume6.webp"; 
 import Resume7 from "./canvaresume7.webp";
-import Resume8 from "./canvaresume8.webp"
+import Resume8 from "./canvaresume8.webp";
+import Resume9 from "./canvaresume9.jpg";
 import CurrentResume from "./CurrentResume/CurrentResume";
 import PreviousResume from "./PreviousResume/PreviousResume";
 import Modal from "./Model";
@@ -99,6 +100,10 @@ function MyResume() {
     {
       url:"https://www.canva.com/p/templates/EAE-TBgRwsI-pink-simple-profile-resume/",
       image:Resume8
+    },
+    {
+      url:"https://www.canva.com/p/templates/EAFxZBGySUs-white-and-brown-vintage-resume/",
+      image:Resume9
     }
   ];
 
