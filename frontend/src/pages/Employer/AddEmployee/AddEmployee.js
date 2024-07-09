387ad1c5d7ea06a15f141c5e9181264e4d6382f6
rishii64 <saptarsismmitra@gmail.com>
2024-07-09 13:18:57 +0530

    image upload functionlity

--- frontend/src/pages/Employer/AddEmployee/AddEmployee.js
+++ frontend/src/pages/Employer/AddEmployee/AddEmployee.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import AddEmp from './Employee.module.css'
-// import { TbCameraPlus } from "react-icons/tb";
 import upload from '../../../Assets/upload.png';
 
 function EmployeeAdd() {
+    const [addStaff, setAddSTaff] = useState({
+        photo: "",
+        name: "",
+        email: "",
+        phone: "",
+        gender: "",
+        role: "",
+        designation: "",
+        id: "",
+        officialMail: "",
+    })
+    const imgRef = useRef()
+
     return (
         <div className={AddEmp.Employee_main}>
             <p className={AddEmp.upper_text}>Add a New Staff</p>
@@ -11,8 +23,11 @@ function EmployeeAdd() {
                 <div className={AddEmp.container1}>
 
                     <div className={AddEmp.__imageContainer}>
-                        <img className={AddEmp.__uploadPhoto} src={upload} alt="" />
-                        <input type="file" accept='image/*' />
+                        <img className={AddEmp.__uploadPhoto} src={addStaff.photo} alt="upload img"
+                            onError={(e) => { e.target.src = `${upload}`; e.onError = null }}
+                            onClick={(e) => imgRef.current.click()} />
+                        {}
+                        <input type="file" accept="image/*" ref={imgRef} hidden name="jobPoster" />
                     </div>
 
                     <div className={AddEmp.detail}>
@@ -25,10 +40,9 @@ function EmployeeAdd() {
                 </div>
 
                 <div className={AddEmp.container2}>
-                    <label for='name'>First name</label>
+                    <label for='name'>Name</label>
                     <input type='text' id='name' placeholder='Enter first name' className={AddEmp.input}></input>
-                    <label for='email'>Email address</label>
-                    <input type='text' id='name' placeholder='Enter email address' className={AddEmp.input}></input>
+
                     <label for='gender'>Gender</label>
                     <select id='gender' className={AddEmp.gender}>
                         <option value="" disabled selected >Select gender</option>
@@ -48,10 +62,8 @@ function EmployeeAdd() {
                 </div>
 
                 <div className={AddEmp.container3}>
-                    <label for='name'>Last name</label>
-                    <input type='text' id='name' placeholder='Enter last name' className={AddEmp.input}></input>
-                    <label for='number'>Phone number</label>
-                    <input type='number' id='number' placeholder='Enter phone number' className={AddEmp.input}></input>
+                    <label for='email'>Email address</label>
+                    <input type='text' id='name' placeholder='Enter email address' className={AddEmp.input}></input>
                     <label for='number'>Phone number</label>
                     <input type='number' id='number' placeholder='Enter phone number' className={AddEmp.input}></input>
                     <label for='dsignation'>Designation</label>
